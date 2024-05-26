1e560c689ef01d049189743d4411d774993c4e65
Nrabyan-17 <naufalrizkiabyan@gmail.com>
2024-05-26 19:53:57 +0700

    add portofolio

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html',
+  ],
+  theme: {
+    extend: {
+      letterSpacing: {
+        wide: '.5em',
+      }
+    },
+  },
+  plugins: [],
+}
+
